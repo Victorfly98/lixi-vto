076372e43c365010d88f8b4431bcf10e61f01114
Hung Nguyen <52589397+hungnguyen2809@users.noreply.github.com>
2022-01-31 21:38:47 +0700

    update video

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 export const config = {
 	audio: "https://mediacloud.viettelpost.vn/2022-01-31/20_35_12-730a08c3-856e-4016-8652-5662d070dd5a.mp3",
-	video: "https://www.youtube.com/watch?v=0SZ0rcdCBTI",
+	video: "https://www.youtube.com/watch?v=vR3hR4ETXf4",
 };
