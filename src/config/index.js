3336c90f06a93d9151f5c2cedf1e9c9a00156fe2
Hung Nguyen <hungnguyen99.nvh@gmail.com>
2022-01-31 20:36:18 +0700

    update audio, video

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 export const config = {
-	audio: "https://res.cloudinary.com/hungnguyen2809/video/upload/v1643522502/lixi-vto/bkoczxwflwifl1cfx2wg.mp3",
-	video: "https://www.youtube.com/watch?v=YGhE2HZ8g8M",
+	audio: "https://mediacloud.viettelpost.vn/2022-01-31/20_35_12-730a08c3-856e-4016-8652-5662d070dd5a.mp3",
+	video: "https://www.youtube.com/watch?v=0SZ0rcdCBTI",
 };
